7e6d0c8a86b2b014ff77d7eea4ed14ebd771cf4d
shiv-coder <shivi359@gmail.com>
2025-01-17 11:32:28 -0500

    Updated Silly Character.js file

--- lesson-2/js/sillyCharacter.js
+++ lesson-2/js/sillyCharacter.js
@@ -34,54 +34,18 @@ display the character's attributes in a creative and humorous way.
 
 // Declare and initialize variables
 
-let characterName = "Alex";
-let age = Math.floor(Math.random() * 100); // Random age between 0 and 100
-let favoriteFood = "Pizza";
-let isSuperhero = Math.random() > 0.5; // Random boolean
-let specialPowers = ["Invisibility", "Super Strength", "Flying"];
+
 
 // Function to generate a random character description
-function generateCharacter() {
-    characterName = `Character ${Math.floor(Math.random()*100)}`;
-    age = Math.floor(Math.random()*100);
-    favoriteFood =["Pizza","Burger","Sushi","Ice Cream"][Math.floor(Math.random()*4)];
-    isSuperher0 = Math.random() > 0.5;
 
-    const powers = specialPowers[Math.floor(Math.random() * specialPowers.length)];
-    console.log(powers);
-    
-    const description = `
-        Meet ${characterName}, a ${age}-year-old ${isSuperhero ? "superhero" : "regular person"} 
-        who loves ${favoriteFood} and has the power of ${powers}!
-    `;
-    document.querySelector("#characterDescription").innerText = description;
-}
 
 // Functions to update character's age
-function increaseAge() {
-    console.log(age);
-    age++;
-    console.log(age);
-    updateCharacterDescription();
-}
 
-function decreaseAge() {
-    console.log(age);
-    age--;
-    console.log(age);
-    updateCharacterDescription();
-}
+
+
 
 // Function to update the character's description after changing age
-function updateCharacterDescription() {
-    const description = `
-        Meet ${characterName}, a ${age}-year-old ${isSuperhero ? "superhero" : "regular person"} 
-        who loves ${favoriteFood} and has the power of ${specialPowers[0]}!
-    `;
-    document.querySelector("#characterDescription").innerText = description;
-}
+
 
 // Add event listeners for buttons using querySelector
-document.querySelector('#generateButton').addEventListener('click', generateCharacter);
-document.querySelector('#increaseAgeButton').addEventListener('click', increaseAge);
-document.querySelector('#decreaseAgeButton').addEventListener('click', decreaseAge);
+
