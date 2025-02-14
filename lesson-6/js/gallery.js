723b29aa0f6c28597f31ff32661b50ee5dee624c
shiv-coder <shivi359@gmail.com>
2025-02-14 11:00:49 -0500

    build event handler foe each imagre

--- lesson-6/js/gallery.js
+++ lesson-6/js/gallery.js
@@ -1,30 +1,47 @@
 // STEP 1: Initialize and declare variables
-
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
 
 /* STEP 2: Loop 5 times to create the <img> elements */
-
+for(let i=1;i<=5;i++){
 		/* STEP 3a: Create a new DOM node - an image element */
-
+	const newImage = document.createElement('img');
 		/* STEP 3b: Set the src attribute to be the path of one of the images inside the images folder, using setAttribute() */
-
+		newImage.setAttribute('src',`images/pic${i}.jpg`);
 		/* Append the new image element to the thumbBar div, named in STEP 1 */
-
+		thumbBar.appendChild(newImage);
 		/* STEP 3c: Build event handler for each <img> */
+		newImage.addEventListener('click',function(event){
+			//console.log('Image Clicked');
+			let imgSrc = event.target.getAttribute('src');
+			console.log(imgSrc);
 
+		})
 
+}
 /* STEP 4: Function to change the src of the main <img> */
 
+function displayImage(value){
+	displayedImage.setAttribute('src',value);
+}
 	// Rewrite the src attribute of the .displayed-img element
 
 
 /* STEP 5: Event Delegation
 Instead of adding event handlers for each image, how about event delegation? Build a click handler on the parent element, and capture each target (and its attributes) from the event object */
+	thumbBar.addEventListener('click',function(event){
+		// event.target is the element that was clicked
 
-	// event.target is the element that was clicked
-
-		// grab the src attribute of the element that was clicked
-
+				if(event.target && event.target.nodeName === 'IMG'){
+					// grab the src attribute of the element that was clicked
+					let imgSrc = event.target.getAttribute('src');
 		// change the main image
+		displayImage(imgSrc);
+
+
+		}
+	})
+	
 		
 
 		// Lab 6 STEP G: Call the clearWayfinding() function that you built below - and enjoy the result!
